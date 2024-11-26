f7c0cd6e4e7294f95f25a9a3724eacac59e81f05
Tamara Elyyan <tamaraelyyan1@gmail.com>
2024-11-26 22:53:38 +0200

    Add files via upload

---
+++ LibraryStore/src/test/java/com/Ecommerce/LibraryStore/LibraryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.LibraryStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
